116d14d57aac4c623c2fc383438ae141c74c479c
clementlefevre <clement.san@gmail.com>
2017-09-03 20:56:09 +0200

    Change timer

--- app/service/sncf_routes.py
+++ app/service/sncf_routes.py
@@ -77,6 +77,7 @@ def add_missing_cols_price(df):
 
 def get_routes(origin, destination, date_trip=datetime.now().isoformat()):
     data = get_data(origin, destination, date_trip)
+    time.sleep(10)
 
     try:
         response = requests.request("POST", URL, data=data, headers=headers)
@@ -87,7 +88,7 @@ def get_routes(origin, destination, date_trip=datetime.now().isoformat()):
         logger.error(e)
         return
 
-    time.sleep(10)
+    
 
     response_dict = response.json()
 
